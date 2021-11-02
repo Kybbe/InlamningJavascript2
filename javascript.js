3859885a283807c8a70b40dca54a24ed6d4d5b42
Jacob Klaren <33457831+Kybbe@users.noreply.github.com>
2021-11-02 15:24:40 +0100

    MVP, absolute basics from instructions
    before adding the JKL touch :)

---
+++ javascript.js
@@ -0,0 +1,28 @@
+/*Här skriver ni koden*/
+var taskBtn = document.getElementById("my-button");
+var taskInput = document.getElementById("task");
+var list = document.getElementById("list");
+var deletes = document.getElementsByClassName("delete");
+
+taskBtn.addEventListener("click", function() {
+  if(taskInput.value != ""){
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(taskInput.value));
+
+    let btn = document.createElement("button");
+    btn.appendChild(document.createTextNode("x"))
+    btn.className = "delete";
+    btn.addEventListener("click", deleteFunction)
+
+    li.appendChild(btn);
+    list.appendChild(li)
+
+    taskInput.value = "";
+    taskInput.focus();
+  }
+});
+
+function deleteFunction() {
+  parent = this.parentElement;
+  list.removeChild(parent)
+}
